840abd9cdfa6f13b4a5b2c50806466756af98a97
Khlnih <doiduykhanls@gmail.com>
2025-03-23 21:00:00 +0700

    add

---
+++ Resident/View/ChangeUserWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Resident.Models;
+
+namespace Resident.View
+{
+    /// <summary>
+    /// Interaction logic for ChangeUserWindow.xaml
+    /// </summary>
+    public partial class ChangeUserWindow : Window
+    {
+        PrnContext _context = new PrnContext();
+        List<User> users;
+        public ChangeUserWindow()
+        {
+            InitializeComponent();
+            ChangeUserWindow_Loaded();
+        }
+        public void ChangeUserWindow_Loaded()
+        {
+            users = new List<User>();
+            dtUserImport.ItemsSource = _context.Users.ToList();
+        }
+        
+    }
+}
